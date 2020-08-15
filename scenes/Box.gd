38924aed83eb30edc5e1e7f56feb719bec311679
Harry Woodworth <harryawoodworth@gmail.com>
2020-08-14 23:51:39 -0400

    Add onClick event to Player and Box

--- scenes/Box.gd
+++ scenes/Box.gd
@@ -8,3 +8,9 @@ func init(game, x, y):
 	game.actor_map[x][y] = sprite_node
 	position = curr_tile * game.TILE_SIZE
 	
+func _input_event(viewport, event, shape_idx):
+		if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
+			on_click()
+			
+func on_click():
+	print("Box clicked!")
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,4 +1,11 @@
-extends Sprite
+extends Area2D
 
 var curr_tile
-onready var tween = $Tween
+onready var tween = $Sprite/Tween
+
+func _input_event(viewport, event, shape_idx):
+		if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
+			on_click()
+			
+func on_click():
+	print("Player clicked!")
