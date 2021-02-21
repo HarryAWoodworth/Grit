5c74277567344a55fbec4554c60470faeaa408c1
Harry Woodworth <harryawoodworth@gmail.com>
2021-02-20 21:12:35 -0500

    Going nowhere with accuracy angle changes

--- scripts/Combat_Manager.gd
+++ scripts/Combat_Manager.gd
@@ -11,7 +11,11 @@ func init():
 func shoot(mouse_position: Vector2, ranged_accuracy_dropoff: int):
 	var b = Bullet.instance()
 	add_child(b)
+	var inaccuracy = rand_range((-1 * ranged_accuracy_dropoff), ranged_accuracy_dropoff)
 	b.position = get_parent().Center_Of_Player.position
-	var direction_to_mouse = b.global_position.direction_to(mouse_position).normalized()
-	b.rotation = direction_to_mouse.angle() + rand_range((-1 * ranged_accuracy_dropoff), ranged_accuracy_dropoff)
-	b.set_direction(direction_to_mouse)
+	var direction_to_mouse = b.global_position.direction_to(mouse_position)
+	direction_to_mouse.x = direction_to_mouse.x + inaccuracy
+	direction_to_mouse.y = direction_to_mouse.y - inaccuracy
+	var vec = direction_to_mouse.normalized()
+	print("DIRECTION_TO_MOUSE: " + str(vec))
+	b.set_direction(vec)
