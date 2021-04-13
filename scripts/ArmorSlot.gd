89fc7b4bc5796162c00c47d70b14fa50ccbd0143
Harry Woodworth <harryawoodworth@gmail.com>
2021-04-13 00:40:56 -0400

    From PC

--- scripts/ArmorSlot.gd
+++ scripts/ArmorSlot.gd
@@ -2,7 +2,29 @@ extends Control
 
 onready var Text = $RichTextLabel
 
-var equipped = true
+# The item equipped in the slot
+var item = null
 
+# Does the slot have an item equipped?
+var equipped = false
+
+# Set text (bbcode)
 func setText(string):
 	Text.bbcode_text = string
+
+func equip(item_):
+	item = item_
+	setText(item.name_specialized)
+	equipped = true
+
+# Set item to null and clear text
+func clear():
+	item = null
+	setText("")
+	equipped = false
+
+# Return the item and clear the slot
+func unequip():
+	var temp = item
+	clear()
+	return temp
